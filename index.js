bfa402377e05d0a7524532df0521208befb02947
Jhonathas Santos <jhondossantos@hotmail.com>
2024-06-30 16:52:10 -0300

    primeiro projeto

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+
+// const lista = [
+
+// carro ='',
+// ano = '',
+
+// info =function(carro , ano ){
+
+//   return console.log(`O Carro ${carro} é do ano ${ano}`)
+// }
+
+
+
+
+
+// ]
+
+
+
+// info(carro = 'gol',ano = 2010)
+
+
+// for(let i = 0 ; i< lista.length; i++){
+
+   
+
+// }
+
+  
+// console.log(lista[id][value])
+
+class Modelo{
+
+  constructor(){
+
+    this.carro = 'Onix'
+    this.ano = 2019
+    
+
+  }
+
+  info(modelo){
+
+      console.log(`O Carro ${modelo} é do ano ${this.ano}`)
+
+
+  }
+
+}
+
+
+const lista = new Modelo
+
+console.log(lista.info('gol'))
